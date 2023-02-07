89048e0b5a5bf844f5bab9776ce2e7d41abcb9c2
SDozH <soldatov_alex@bk.ru>
2023-02-08 00:16:21 +0300

    Добавьте файлы проекта.

---
+++ SocialNetworkWeb/Controllers/AccountManagerController.cs
@@ -0,0 +1,22 @@
+﻿
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SocialNetworkWeb.Controllers
+{
+    public class AccountManagerController : Controller
+    {
+        private IMapper _mapper;
+
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountManagerController(UserManager<User> userManager, SignInManager<User> signInManager, IMapper mapper)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _mapper = mapper;
+        }
+    }
+}
---
+++ SocialNetworkWeb/Controllers/RegisterController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialNetworkWeb.ViewModels.Account;
+
+namespace SocialNetworkWeb.Controllers
+{
+    public class RegisterController : Controller
+    {
+        [Route("Register")]
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View("Home/Register");
+        }
+
+        [Route("RegisterPart2")]
+        [HttpGet]
+        public IActionResult RegisterPart2(RegisterViewModel model)
+        {
+            return View("RegisterPart2", model);
+        }
+    }
+}
+

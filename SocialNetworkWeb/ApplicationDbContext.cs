1ef711072cc82b219f21b935773176129278c115
SDozH <soldatov_alex@bk.ru>
2023-02-08 00:16:21 +0300

    Добавьте файлы проекта.

---
+++ SocialNetworkWeb/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using IdentityDbContext = Microsoft.EntityFrameworkCore.DbContext;
+
+
+
+namespace SocialNetworkWeb
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}

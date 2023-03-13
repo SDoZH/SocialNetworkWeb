115ff5ab1edc5c84af57d0ba797652f22c3dbfe1
SDozH <soldatov_alex@bk.ru>
2023-03-14 00:11:08 +0300

    2

---
+++ SocialNetworkWeb/ViewModels/Account/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialNetworkWeb.ViewModels.Account
+{
+    public class LoginViewModel
+    {
+      
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        [StringLength(100, ErrorMessage = "Поле {0} должно иметь минимум {2} и максимум {1} символов.", MinimumLength = 5)]
+        public string Password { get; set; }
+        public bool RememberMe { get; internal set; }
+        public string ReturnUrl { get; internal set; }
+    }
+}
---
+++ SocialNetworkWeb/ViewModels/Months.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialNetworkWeb.ViewModels
+{
+    public enum Months
+    {
+        [Display(Name = "Январь")]
+        Jan = 1,
+        [Display(Name = "Февраль")]
+        Feb = 2,
+        [Display(Name = "Март")]
+        Mar = 3,
+        Apr = 4,
+        May = 5,
+        June = 6,       
+        Jul = 7,
+        Aug = 8,
+        September = 9,      
+        October = 10,
+        November = 11,
+        December = 12,    
+    }
+}
